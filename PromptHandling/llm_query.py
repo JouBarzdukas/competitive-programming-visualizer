a93014e25d7cbe6e70cf3bdf3d9755d3d1033332
Jou Barzdukas <joubarzdukas@gmail.com>
2025-04-01 08:26:50 -0400

    Added next step in instruction logic for step-by-step instructions

--- PromptHandling/llm_query.py
+++ PromptHandling/llm_query.py
@@ -1,9 +1,17 @@
 import openai
 
-def get_LLM_response(prompt) -> str:
+def get_coding_LLM_response(prompt) -> str:
     response = openai.responses.create(
         model="gpt-4o-mini",
-        instructions="Write the python code for the following problem which starts at def and ends at the return statement. This should have NO comments. After that, write potential variables for a generic case in one line. For example, if a function had three parameters, this COULD be a correct choice: ""var1 = [3,5,1,6,7,1,2], var2 = [3,2,8,1], k = 3"". Do not include any explanation.",
+        instructions="Write the python code for the following problem which starts at def and ends at the return statement. This should have NO comments. After that, write potential variables for a generic case in one line. For example, if a function had three parameters, this COULD be a correct choice: ""var1 = [3,5,1,6,7,1,2], var2 = [3,2,8,1], k = 3"". However, use the same variable names as the ones in the example: if def method(s), you should use write ""s = "" in the test case. Do not include any explanation.",
+        input=prompt,
+    )
+    return response.output_text
+
+def get_step_by_step_instructions(prompt) -> str:
+    response = openai.responses.create(
+        model="gpt-4o-mini",
+        instructions="Suppose you are an animator's assistant which specialises in education. You are tasked with writing code which shows exactly what is going on given a method and some test variables. The animator needs you to give step by step instructions on what digital movements should be going on to best make this animation work. By that, I mean you should state EXACTLY what should be happening at each step and telling me HOW it should be animated. Start each step with ""Step X:"" where X is the step number.",
         input=prompt,
     )
     return response.output_text
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PromptHandling.llm_query import get_LLM_response
+from PromptHandling.llm_query import get_coding_LLM_response
+from PromptHandling.llm_query import get_step_by_step_instructions
 from PromptHandling.prompt_parsing import parse_output
 from dotenv import load_dotenv
 import openai
@@ -11,10 +12,15 @@ def main():
 
     additional_instructions = "Complete the following code: " #Might add some later, but system seems to do similar thing (in LLM_query)
     prompt = additional_instructions + inputs
-    raw_output = get_LLM_response(prompt)
+
+    # Ask how to do coding problem
+    raw_output = get_coding_LLM_response(prompt)
     generated_code, test_case = parse_output(raw_output)
-    print("Generated Code:" + generated_code)
-    print("Test Case:" + test_case)
+
+    # Get step by step instructions for coding problem
+    raw_output = get_step_by_step_instructions(generated_code)
+
+    
 
 if __name__ == "__main__":
     load_dotenv()
